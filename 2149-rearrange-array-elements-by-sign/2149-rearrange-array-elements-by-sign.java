7e9fa3402477c6bd54e7de2e0014c2744218f500
Himanshi Singla <singlahimanshi2@gmail.com>
2026-09-08 11:51:29 +0530

    Time: 9 ms (11.4%), Space: 174.6 MB (82.49%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -2,22 +2,59 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         // brute force
         // create two separate arrays one for positive and one for negative 
-        int n = nums.length;
+        // if the number of positives = number of negatives 
+        // int n = nums.length;
+        // ArrayList<Integer> pos = new ArrayList<>();
+        // ArrayList<Integer> neg = new ArrayList<>();
+
+        // for( int i = 0 ; i < nums.length ; i++){
+        //     if( nums[i] > 0 ){
+        //         pos.add(nums[i]);
+        //     }else{
+        //         neg.add(nums[i]);
+        //     }
+        // }
+
+        // int index = 0;
+        // for( int i = 0 ; i < pos.size() ; i++){
+        //     nums[index++] = pos.get(i);
+        //     nums[index++] = neg.get(i);
+        // }
+
+        // return nums;
+
+        // if number of positives != number of negatives 
         ArrayList<Integer> pos = new ArrayList<>();
         ArrayList<Integer> neg = new ArrayList<>();
 
         for( int i = 0 ; i < nums.length ; i++){
-            if( nums[i] > 0 ){
+            if( nums[i] > 0){
                 pos.add(nums[i]);
             }else{
                 neg.add(nums[i]);
             }
-        }
+        } 
 
-        int index = 0;
-        for( int i = 0 ; i < pos.size() ; i++){
-            nums[index++] = pos.get(i);
-            nums[index++] = neg.get(i);
+        if( pos.size() >= neg.size() ){
+            for( int i = 0 ; i < neg.size(); i++){
+                nums[2*i] = pos.get(i);
+                nums[2*i+1] = neg.get(i);
+            }
+            int index = neg.size() * 2;
+            // fill the leftover elements of positive arraylist
+            for( int i = neg.size() ; i < pos.size(); i++){
+                nums[index++] = pos.get(i); 
+            }
+        }else{
+            for( int i = 0 ; i < pos.size(); i++){
+                nums[2*i] = pos.get(i);
+                nums[2*i+1] = neg.get(i);
+            }
+            int index = pos.size() * 2;
+            // fill the leftover elements of positive arraylist
+            for( int i = pos.size() ; i < neg.size(); i++){
+                nums[index++] = neg.get(i); 
+            }
         }
 
         return nums;
